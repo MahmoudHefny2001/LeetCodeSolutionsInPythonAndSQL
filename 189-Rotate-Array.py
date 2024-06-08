bf9b134e7f99d1f8d4c6d84f248efe7823405384
Mahmoud Hefny <66621768+MahmoudHefny2001@users.noreply.github.com>
2024-06-08 03:28:49 +0300

    [189] [Time Beats: 42.10%] [Memory Beats: 61.46%] - LeetPush

--- 189-Rotate-Array.py
+++ 189-Rotate-Array.py
@@ -1,28 +1,29 @@
 class Solution:
+
+    def helper(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
 
-        if len(nums) == 1 or len(nums) == 0:
-            nums = nums
-            return
-
-        # Create a new list with the same length as nums
-        new = [0] * len(nums)
-
-        k = k % (len(nums))
-
-        j = k
-
-        left = len(nums) - 1
-
-        for i in range(len(nums)):
-            new[ (i + k) % len(nums) ] = nums[i]
+        k = k % len(nums)
         
-        nums[:] = new
+        l, r = 0, len(nums) - 1
+        
+        self.helper(nums, l, r)
         
 
+        l, r = 0, k - 1
+        
+        self.helper(nums, l, r)
 
+
+        l, r = k, len(nums) - 1
+        
+        self.helper(nums, l, r)
 
 
